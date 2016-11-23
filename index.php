19625b13c3f239a5d2ba240babb6bf382919e657
Vanderlan <vanderlan.gs@gmail.com>
2016-11-22 23:07:02 -0300

    Adição do médoto de listagem de NFe por período

--- index.php
+++ index.php
@@ -1,24 +1,58 @@
 <?php
 
-$login = 'usumobdesenv';
-$password = 'passdesenv123';
+
 $loginAppp = '45.922.746/0001-90';
 $passwordApp = '123456';
 
-$loginUrl = 'http://usve79048.serverprofi24.com/eiss/webservice/ws_dadoslogin.php';
+//TESTE DE RETORNO
+//echo loginAplicacao($loginAppp, $passwordApp);
 
-$ch = curl_init();
+function loginAplicacao($loginAppp, $passwordApp)
+{
 
-curl_setopt($ch, CURLOPT_URL, $loginUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'login='.$login.'&senha='.$password.'&loginApp='.$loginAppp.'&senhaApp='.$passwordApp);
-curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$login = 'usumobdesenv';
+		$password = 'passdesenv123';
+		
+		$loginUrl = 'http://usve79048.serverprofi24.com/eiss/webservice/ws_dadoslogin.php';
 
-$content = curl_exec($ch);
+		$ch = curl_init();
 
-curl_close($ch);
+		curl_setopt($ch, CURLOPT_URL, $loginUrl);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, 'login='.$login.'&senha='.$password.'&loginApp='.$loginAppp.'&senhaApp='.$passwordApp);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-echo $content;
+		$content = curl_exec($ch);
 
+		curl_close($ch);
+
+		return $content;
+}
+
+$periodo = '2016-02';
+
+function listarNotasGeradas($loginAppp, $passwordApp, $periodo)
+{
+
+		$login = 'usumobdesenv';
+		$password = 'passdesenv123';
+		
+		//ESTA URL NÃO ESTÁ DISPONÍVEL
+		$loginUrl = 'http://usve49032.serverprofi24.com/eiss/webservice/ws_buscatomador.php';
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $loginUrl);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, 'login='.$login.'&senha='.$password.'&loginApp='.$loginAppp.'&senhaApp='
+			.$passwordApp .'&periodo='.$periodo);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		$content = curl_exec($ch);
+
+		curl_close($ch);
+
+		return $content;
+
+}
 ?>
